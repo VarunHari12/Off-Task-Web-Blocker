dbb7ec08f346fc55c10bbe25a62e7059c224e2e4
VarunHari12 <varunhari@outlook.com>
2024-01-02 17:28:31 -0800

    made comments deleted background function

--- background.js
+++ background.js
@@ -1,13 +1,4 @@
-chrome.runtime.onStartup.addListener(function() {
-    
-    chrome.tabs.create({ url: chrome.runtime.getURL("popup.html") }, function(tab) {
-        
-    });
+// opens popup when the web browser starts
+chrome.runtime.onStartup.addListener(function() {   
+    chrome.tabs.create({ url: chrome.runtime.getURL("popup.html") });
   });
-
-  //chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    //if (request.action === "setLocalStorage") {
-        //localStorage.setItem("intent", request.data);
-        //console.log(localStorage.getItem("intent"));
-    //}
-//});
