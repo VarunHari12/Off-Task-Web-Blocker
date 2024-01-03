ae722a59736d91cda12f5f9516c3a8382914adf3
VarunHari12 <varunhari@outlook.com>
2024-01-02 17:36:42 -0800

    added commments

--- popup.js
+++ popup.js
@@ -1,20 +1,12 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('submitButton').addEventListener('click', function () {
+    document.getElementById('submitButton').addEventListener('click', function () { // adds submit button to intent page
         getIntent();
     });
 });
 
+// grabs the user intent and stores it
 function getIntent() {
     const userInput = document.getElementById('userInput').value;
-    //console.log("User intent:", userInput);  
-    chrome.storage.sync.set({"intent": userInput}, function() {
-        //console.log('User intent set to ' + userInput);
-      });
-    
-      //chrome.storage.sync.get(["intent"], function(result) {
-        //alert('Value currently is ' + result.intent);
-      //});
-    
+    chrome.storage.sync.set({"intent": userInput});
     window.close();
 }
